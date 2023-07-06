0f3304561d0420940f0f1f69f1ca1e2194bcea70
CanerCagri <canerrcagrii@gmail.com>
2023-07-06 20:21:25 +0300

    Initial Commit

---
+++ SwiftUI-Network-Call/SwiftUI_Network_CallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Network_CallApp.swift
+//  SwiftUI-Network-Call
+//
+//  Created by Caner Çağrı on 6.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Network_CallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
